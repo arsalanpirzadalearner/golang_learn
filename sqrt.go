a344f4acd8a3460100f9f015093be3179e603e1a
arsalanpirzadalearner <notifications1@arsalanpirzada.com>
2018-08-28 04:51:25 +0500

    square roots

---
+++ sqrt.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	zprev := z
+	z -= (z*z - x) / (2 * z)
+
+	for z != zprev {
+		zprev = z
+		z -= (z*z - x) / (2 * z)
+		// fmt.Println(z)
+		// fmt.Println("XXX")
+	}
+	return zprev
+}
+
+func main() {
+	fmt.Println(Sqrt(144))
+}
